d4b9f39f5e731bfb1dd9c5c022e69bc110fe5c6e
MARWAN SAMEER <aa048587@gmail.com>
2025-07-02 13:32:45 +0300

    edits

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -44,7 +44,8 @@
   <div class="max-w-7xl mx-auto px-6 lg:px-8 h-full flex items-center justify-between">
     {{-- Logo --}}
     <a href="{{ route('home') }}" class="flex items-center space-x-2 hover:scale-105 transition-transform duration-200">
-      <img src="/images/ChatGPT Image 2 يوليو 2025، 12_55_08 م.png" alt="Logo" class="h-20 w-20">
+      <img src="/images/ChatGPT Image 2 يوليو 2025، 12_55_08 م.png" alt="Logo" class="h-20 w-20 block dark:hidden">
+      <img src="/images/WhatsApp_Image_2025-07-01_at_9.33.32_PM-removebg-preview.png" alt="Logo Dark" class="h-20 w-20 hidden dark:block">
       {{-- <span class="text-2xl font-extrabold text-gray-900 dark:text-gray-100">
         {{ __('messages.site_name') }}
       </span> --}}
