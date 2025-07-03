e9c0451a43c03dc60d72c12f768461266f24c9fd
MARWAN SAMEER <aa048587@gmail.com>
2025-07-03 14:27:18 +0300

    ثيهفس

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -60,13 +60,13 @@
           </p>
           <a
             href="{{ route('categories') }}"
-            class="group inline-flex items-center bg-[#fcc85e] text-gray-900 font-semibold py-3 px-10 rounded-full shadow-lg transition-all duration-300 hover:scale-105 hover:shadow-xl hover:bg-[#fdd17a] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-[#fcc85e]"
+            class="group inline-flex items-center justify-center overflow-hidden bg-[#fcc85e] text-gray-900 font-semibold py-3 px-10 rounded-full shadow-lg transition-all duration-300 hover:scale-105 hover:shadow-xl hover:bg-[#fdd17a] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-[#fcc85e]"
           >
-            {{ __('messages.cats') }}
-            <svg xmlns="http://www.w3.org/2000/svg"
-                 class="ml-3 h-5 w-5 text-gray-900 transition-transform duration-300 group-hover:translate-x-1 {{ app()->getLocale() === 'ar' ? 'rotate-180' : '' }}"
+            <span>{{ __('messages.cats') }}</span>
+            <svg xmlns="http://www.w3.org/2000/svg" 
+                 class="h-6 w-0 opacity-0 transition-all duration-300 group-hover:w-6 group-hover:opacity-100 rtl:rotate-180 ml-0 group-hover:ml-2"
                  fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-              <path stroke-linecap="round" stroke-linejoin="round" d="M17 8l4 4m0 0l-4 4m4-4H3" />
+              <path stroke-linecap="round" stroke-linejoin="round" d="M13 7l5 5-5 5M6 12h12" />
             </svg>
           </a>
         </div>
